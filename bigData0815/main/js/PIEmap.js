fc4e939b57d1c0faf34def50c62884ad2bbddade
EZreal-Map <tangkaiwen00@gmail.com>
2023-04-28 00:56:23 +0800

    增加水深颜色带的显示，代替原来riverSection全部为BLUE，现在riverSection的断面水深从高到低分为10个等级，对应的红到蓝10个色带的颜色

--- bigData0815/main/js/PIEmap.js
+++ bigData0815/main/js/PIEmap.js
@@ -23,8 +23,7 @@ createEarthModule().then(function () {
             index: 0,
           }),
     });
-    console.log(viewer.imageryLayers._imageryLayers[0])
-    console.log(viewer.imageryLayers.indexOf(viewer.imageryLayers._imageryLayers[0]))
+
     // let ImageryProvider_imagery = new Earth.ArcGisMapServerImageryProvider({
     //     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     //     epsg: 3857,
@@ -425,11 +424,11 @@ createEarthModule().then(function () {
             if (pickObj.polygon) {
                 if (pickObj.labelBox.show) {
                     // pickObj.polygon.color = Earth.Color.fromBytes(255, 0, 0, 200);
-                    pickObj.polygon.color = Earth.Color.RED;
+                    pickObj.polygon.color = Earth.Color.WHITE.withAlpha(0.8);
                 }
                 else {
                     // pickObj.polygon.color = Earth.Color.fromBytes(0, 0, 255, 200);
-                    pickObj.polygon.color = Earth.Color.BLUE;
+                    pickObj.polygon.color = pickObj.colorNotation;
                 }
             }
 
@@ -512,6 +511,88 @@ function getPolygonCentroid(points) {
 //             }
 //         });
 // }
+function gradientColor(startColor, endColor, step) {
+    startRGB = colorToRgb(startColor); //转换为rgb数组模式
+    startR = startRGB[0];
+    startG = startRGB[1];
+    startB = startRGB[2];
+
+    endRGB = colorToRgb(endColor);
+    endR = endRGB[0];
+    endG = endRGB[1];
+    endB = endRGB[2];
+
+    sR = (endR - startR) / step; //总差值
+    sG = (endG - startG) / step;
+    sB = (endB - startB) / step;
+
+    var colorArr = [];
+    for(var i = 0; i < step; i++) {
+        //计算每一步的hex值
+        var hex = colorToHex('rgb(' + parseInt((sR * i + startR)) + ',' + parseInt((sG * i + startG)) + ',' + parseInt((sB * i + startB)) + ')');
+        colorArr.push(hex);
+    }
+    return colorArr;
+}
+
+// 将hex表示方式转换为rgb表示方式(这里返回rgb数组模式)
+function colorToRgb(sColor) {
+    var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+    var sColor = sColor.toLowerCase();
+    if(sColor && reg.test(sColor)) {
+        if(sColor.length === 4) {
+            var sColorNew = "#";
+            for(var i = 1; i < 4; i += 1) {
+                sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+            }
+            sColor = sColorNew;
+        }
+        //处理六位的颜色值
+        var sColorChange = [];
+        for(var i = 1; i < 7; i += 2) {
+            sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+        }
+        return sColorChange;
+    } else {
+        return sColor;
+    }
+};
+
+// 将rgb表示方式转换为hex表示方式
+function colorToHex(rgb) {
+    var _this = rgb;
+    var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+    if(/^(rgb|RGB)/.test(_this)) {
+        var aColor = _this.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
+        var strHex = "#";
+        for(var i = 0; i < aColor.length; i++) {
+            var hex = Number(aColor[i]).toString(16);
+            hex = hex < 10 ? 0 + '' + hex : hex; // 保证每个rgb的值为2位
+            if(hex === "0") {
+                hex += hex;
+            }
+            strHex += hex;
+        }
+        if(strHex.length !== 7) {
+            strHex = _this;
+        }
+
+        return strHex;
+    } else if(reg.test(_this)) {
+        var aNum = _this.replace(/#/, "").split("");
+        if(aNum.length === 6) {
+            return _this;
+        } else if(aNum.length === 3) {
+            var numHex = "#";
+            for(var i = 0; i < aNum.length; i += 1) {
+                numHex += (aNum[i] + aNum[i]);
+            }
+            return numHex;
+        }
+    } else {
+        return _this;
+    }
+}
 
 function refreshMapRiverShow() {
     fetch('./riversection.geojson')
@@ -519,6 +600,14 @@ function refreshMapRiverShow() {
         .then(data => {
             // console.log(data.features); // data为JSON对象
             var tmpObjs = data.features;
+            
+            // 色带
+            var gradient = gradientColor('#0000FF', '#FF0000', 10)
+
+            // 记录最小、最大水深
+            var m_GradientColorMax = Math.max.apply(null,m_RiverSectionVDataArray[3]);
+            var m_GradientColorMin = Math.min.apply(null,m_RiverSectionVDataArray[3]);
+            var m_GradientColorStep = (m_GradientColorMax - m_GradientColorMin) / 10;
 
             for (var i in tmpObjs) {
                 tmpObj = tmpObjs[i];
@@ -538,18 +627,26 @@ function refreshMapRiverShow() {
                 var tmpRiverSectionWaterLevel = m_RiverSectionVDataArray[0][i].toFixed(2);
                 var tmpRiverSectionWaterDepth = m_RiverSectionVDataArray[3][i].toFixed(2);
 
+                var tmpI = parseInt((tmpRiverSectionWaterDepth - m_GradientColorMin) / m_GradientColorStep);
+                tmpI = tmpI > 9 ? 9:tmpI;
+                var colorNotation = Earth.Color.fromCssColorString(gradient[tmpI]);
+
+
                 const polygon = new Earth.Entity({
                     name: tmpNickName,
                     show: true,
                     layer: 'riverSectionLayer',
                     // zIndex: 5,
+                    // waterDepth: tmpRiverSectionWaterDepth, 暂时没有需要
+                    colorNotation: colorNotation,
                     position: Earth.Cartesian3.fromDegrees(polygonCentroid[0], polygonCentroid[1]), //经度，纬度，高度
 
                     polygon: new Earth.Polygon({
                         show: true,
                         hierarchy: Earth.Cartesian3.fromDegreesArray(tmpCoordinates),
                         // color: Earth.Color.fromBytes(0, 0, 255, 255),
-                        color: Earth.Color.BLUE,
+                        // color: Earth.Color.BLUE,
+                        color: colorNotation,
                         heightReference: Earth.HeightReference.CLAMP_TO_GROUND, //贴地
                         // height: -100,
                         outline: true, //好像暂时不支持
@@ -805,7 +902,7 @@ $(document).ready(function () {
 			entityCollection._entities[i].labelBox.show = false;
 			if (entityCollection._entities[i].polygon) {
 				// entityCollection._entities[i].polygon.color = Earth.Color.fromBytes(0, 0, 255, 200);
-				entityCollection._entities[i].polygon.color = Earth.Color.BLUE;
+				entityCollection._entities[i].polygon.color = entityCollection._entities[i].colorNotation;
 
 			}
 		}
@@ -947,6 +1044,18 @@ $(document).ready(function () {
 	// 		}
 	// 	}
 	// }
+
+
+
+    // 颜色渐变函数 gradientColor(startColor, endColor, step)，用于生成从 startColor 到 endColor 的颜色渐变过程，分成了 step 步
+    // 1、 将 startColor 和 endColor 两个颜色值转换成 rgb 数组格式，方便后续计算。
+    // 2、计算每一步颜色的 rgb 值，将其转换成十六进制表示。
+    // 3、将每一步的十六进制颜色值保存到数组 colorArr 中。
+    // 4、返回 colorArr 数组。
+
+    
+
+
 })
 
 
